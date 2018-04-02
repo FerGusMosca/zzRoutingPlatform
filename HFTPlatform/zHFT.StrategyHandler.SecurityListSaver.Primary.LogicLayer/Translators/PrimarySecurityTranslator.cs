3dfa8a27f56952e95bc7bca539d16931e102d6ae
Zimzum <alien.zimzum@gmail.com>
2018-04-02 19:21:49 -0300

    Correccion de bug en la carga del symbol de los futuros (faltaba el mercado)
    Former-commit-id: 7c0bcd9b6e2d0756eb36ae780b19df54bcf79beb

--- HFTPlatform/zHFT.StrategyHandler.SecurityListSaver.Primary.LogicLayer/Translators/PrimarySecurityTranslator.cs
+++ HFTPlatform/zHFT.StrategyHandler.SecurityListSaver.Primary.LogicLayer/Translators/PrimarySecurityTranslator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using zHFT.Main.BusinessEntities.Securities;
+using zHFT.Main.Common.Enums;
 using zHFT.StrategyHandler.SecurityListSaver.Common.Interfaces;
 
 namespace zHFT.StrategyHandler.SecurityListSaver.Primary.LogicLayer.Translators
@@ -13,6 +14,7 @@ namespace zHFT.StrategyHandler.SecurityListSaver.Primary.LogicLayer.Translators
         #region Private static Consts
 
         private string _PRIMARY_BYMA_CODE = "MERV";
+        private string _PRIMARY_ROFX_CODE = "ROFX";
         private string _SECURITIES_HISTORICAL_DATA_BYMA_CODE = "BUE";
 
         #endregion
@@ -30,6 +32,12 @@ namespace zHFT.StrategyHandler.SecurityListSaver.Primary.LogicLayer.Translators
 
                 if (sec.Exchange.Trim() == _PRIMARY_BYMA_CODE)
                     sec.Exchange = _SECURITIES_HISTORICAL_DATA_BYMA_CODE;
+
+                if (sec.SecType == SecurityType.FUT)
+                {
+                    if (sec.Exchange.Trim() == _PRIMARY_ROFX_CODE)
+                        sec.Symbol = sec.Symbol + "." + _PRIMARY_ROFX_CODE;
+                }
 
                 //Los demas códigos no existen en el mundo SecuritiesHistoricalData así que 
                 // por ahora no lo traducimos
