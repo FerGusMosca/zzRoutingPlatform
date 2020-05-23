c0f1f0164c0f03671d2e05302a71f5aff7123c1b
Fernando Mosca <fernando.mosca@tradehelm.com>
2020-05-23 12:19:10 -0300

    Bitfinex integration v1
    Former-commit-id: cc8a81f5b6133c380db25c392bb3b42be9dbaa35

---
+++ HFTPlatform/zHFT.StrategyHandler.IBR.Bitfinex.DAL/BitfinexAccountManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zHFT.StrategyHandler.InstructionBasedRouting.BusinessEntities;
+using zHFT.StrategyHandler.InstructionBasedRouting.Common.Interfaces;
+
+namespace zHFT.StrategyHandler.IBR.Bitfinex.DAL
+{
+    public class BitfinexAccountManager : IAccountReferenceHandler
+    {
+        #region Protected Attributes
+
+        protected bool ReqAccountSummary { get; set; }
+
+        protected bool ReqAccountPositions { get; set; }
+
+        protected bool AbortOnTimeout { get; set; }
+
+        #endregion
+
+
+        #region Public Methods
+
+        public bool SyncAccountPositions(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SyncAccountBalance(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ReadyAccountSummary()
+        {
+            return ReqAccountSummary;
+        }
+
+        public bool WaitingAccountPositions()
+        {
+            return ReqAccountPositions;
+        }
+
+        public bool IsAbortOnTimeout()
+        {
+            return AbortOnTimeout;
+        }
+
+        public Account GetAccountToSync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountPosition> GetActivePositions()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
