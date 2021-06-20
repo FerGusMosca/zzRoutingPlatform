5d0cab0f769058e50b382532b596ca4f48f67646
FerGusMos <fernando.mosca@tradehelm.com>
2021-06-20 18:19:02 -0300

    orpheums library and others
    Former-commit-id: 35d8ecc1ab6f9cfdf55f487a2a0fc158ffe924e1

---
+++ HFTPlatform/tph.StrategyHandler.SimpleCommandReceiver/SimpleCommandReceiver.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Fleck;
+using zHFT.Main.Common.Abstract;
+using zHFT.Main.Common.Configuration;
+using zHFT.Main.Common.DTO;
+using zHFT.Main.Common.Interfaces;
+using zHFT.Main.Common.Util;
+using zHFT.Main.Common.Wrappers;
+using zHFT.StrategyHandler.LogicLayer;
+
+namespace tph.StrategyHandler.SimpleCommandReceiver
+{
+    public class SimpleCommandReceiver: BaseCommunicationModule
+    {
+        #region Protected Attributes
+        
+        protected WebSocketServer Server { get; set; }
+        
+        public tph.StrategyHandler.SimpleCommandReceiver.Common.Configuration.Configuration Config { get; set; }
+        
+        #endregion
+        
+        #region ICommunicationModule
+
+        protected override void DoLoadConfig(string configFile, List<string> noValFields)
+        {
+            List<string> noValueFields = new List<string>();
+            Config = new Configuration().GetConfiguration<tph.StrategyHandler.SimpleCommandReceiver.Common.Configuration.Configuration>(configFile, noValueFields);
+        }
+
+        public override CMState ProcessMessage(Wrapper wrapper)
+        {
+            DoLog(string.Format("Invoking ProcessMessage @SimpleCommandReceiver which is not implemented"),Constants.MessageType.Error);
+            return CMState.BuildSuccess();
+        }
+
+        public override bool Initialize(OnMessageReceived pOnMessageRcv, OnLogMessage pOnLogMsg, string configFile)
+        {
+            try
+            {
+                this.OnMessageRcv += pOnMessageRcv;
+                this.OnLogMsg += pOnLogMsg;
+
+                if (LoadConfig(configFile))
+                {
+                    //Finish starting up the server
+//                    Server = new WebSocketServer(Configuration.WebSocketAdddress, this, OnMessageRcv);
+//                    Server.Start();
+
+                    DoLog("Websocket successfully initialized on URL:  " + Config.WebSocketURL, Constants.MessageType.Information);
+                    return true;
+                }
+                else
+                {
+                    DoLog("Error initializing config file " + configFile, Constants.MessageType.Error);
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                DoLog("Critic error initializing " + configFile + ":" + ex.Message, Constants.MessageType.Error);
+                return false;
+            }
+        }
+        
+        #endregion
+
+        
+    }
+}
