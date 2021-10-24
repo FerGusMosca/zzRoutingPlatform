bfff22e7bc060b87b1ca5935972faa5bf0811938
FerGusMos <fernando.mosca@tradehelm.com>
2021-10-24 19:37:37 -0300

    uncomplete pos sync
    Former-commit-id: 4b2d7431c9e6465c1fee78be39e14fa75c8680c7

---
+++ HFTPlatform/tph.StrategyHandler.IBR.Cocos.ServiceLayer/BaseServiceClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+
+namespace tph.StrategyHandler.IBR.Cocos.ServiceLayer
+{
+    public class BaseServiceClient
+    {
+        #region Protected Consts
+
+        protected string _LOGIN_URL = "/Login/Ingresar";
+
+        protected string _VALIDATE_NEW_ORDER_ASYNC = "/Order/ValidarCargaOrdenAsync";
+
+        #endregion
+        
+        #region Protected Attributes
+        
+        protected HttpClientHandler CookieHandler { get; set; }
+        
+        protected object tAuthLock { get; set;}
+        
+        protected string BaseURL { get; set; }
+        
+        public string DNI { get; set; }
+        
+        protected string User { get; set; }
+        
+        protected  string Password { get; set; }
+        
+        
+        #endregion
+        
+        #region Constructors
+
+        public BaseServiceClient()
+        {
+            tAuthLock=new object();
+        }
+    
+        
+        #endregion
+        
+        #region Protected Methods
+        
+        //if there are error, exceptions will be thrown
+        protected void DoAuthenticate()
+        {
+            string url = string.Format("{0}{1}", BaseURL, _LOGIN_URL);
+            
+            Dictionary<string, string> queryString =  new Dictionary<string, string>();
+            queryString.Add("Dni", DNI);
+            queryString.Add("Usuario", User);
+            queryString.Add("Password", Password);
+            
+            //CookieHandler
+
+            if (CookieHandler == null)
+            {
+                CookieContainer cookies = new CookieContainer();
+                CookieHandler = new HttpClientHandler();
+                CookieHandler.CookieContainer = cookies;
+            }
+
+            string resp = DoPostJson(url, queryString);
+
+            if (!resp.Contains("usuarioLogueado"))
+                throw new Exception(string.Format("Could not authenticate user {0} and DNI {1}", User, DNI));
+
+        }
+
+        protected string DoPostJson(string url, IEnumerable<KeyValuePair<string, string>> postData)
+        {
+
+            lock (tAuthLock)
+            {
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls12 |
+                                                       SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+                
+                using (var httpClient = new HttpClient(CookieHandler,false))
+                {
+                    using (var content = new FormUrlEncodedContent(postData))
+                    {
+                        content.Headers.Clear();
+                        content.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+
+                        HttpResponseMessage response =  httpClient.PostAsync(url, content).Result;
+
+                        string resp =  response.Content.ReadAsStringAsync().Result;
+
+                        return resp;
+                    }
+                }
+            }
+
+        }
+
+        #endregion
+    }
+}
---
+++ HFTPlatform/tph.StrategyHandler.IBR.Cocos.ServiceLayer/CocosAccountServiceClient.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using zHFT.Main.Common.Interfaces;
+using zHFT.Main.Common.Util;
+using zHFT.StrategyHandler.InstructionBasedRouting.BusinessEntities;
+using zHFT.StrategyHandler.InstructionBasedRouting.Common.Configuration;
+using zHFT.StrategyHandler.InstructionBasedRouting.Common.Interfaces;
+
+namespace tph.StrategyHandler.IBR.Cocos.ServiceLayer
+{
+    public class CocosAccountClient: BaseServiceClient,IAccountReferenceHandler
+    {
+        
+        #region Constructors
+
+        public CocosAccountClient(OnLogMessage OnLogMsg, List<ConfigKey> pConfigParameters)
+        {
+            Name = "Invertir Online Account Reference Handler";
+            Logger = OnLogMsg;
+            ReqAccountSummary = false;
+            ReqAccountPositions = false;
+            AbortOnTimeout = false;
+            AccountToSync = new Account();
+            Positions = new List<AccountPosition>();
+            
+            ConfigParameters = pConfigParameters;
+
+            Logger("Authenticating Account Manager On Cocos",Constants.MessageType.Information);
+            DoAuthenticate();
+            Logger(string.Format("Account Manager authenticated On Cocos"),Constants.MessageType.Information);
+        }
+
+        #endregion
+        
+        #region Protected Attributes
+        
+        protected List<ConfigKey> ConfigParameters { get; set; }
+        
+        protected Boolean ReqAccountSummary { get; set; }
+
+        protected Boolean ReqAccountPositions { get; set; }
+        
+        protected string Name { get; set; }
+
+        protected OnLogMessage Logger { get; set; }
+        
+        protected bool AbortOnTimeout { get; set; }
+        
+        public Account AccountToSync { get; set; }
+        
+        protected List<AccountPosition> Positions { get; set; }
+        
+        #endregion
+        
+        #region Public Attributes
+        
+        public bool SyncAccountPositions(Account account)
+        {
+            //TODO: dev sync
+
+            return true;
+        }
+
+        public bool SyncAccountBalance(Account account)
+        {
+            //TODO: dev sync
+
+            return true;
+        }
+
+        public bool ReadyAccountSummary()
+        {
+            return false;
+        }
+
+        public bool WaitingAccountPositions()
+        {
+            return false;
+        }
+
+        public bool IsAbortOnTimeout()
+        {
+            return AbortOnTimeout;
+        }
+
+        public Account GetAccountToSync()
+        {
+            return AccountToSync;
+        }
+
+        public List<AccountPosition> GetActivePositions()
+        {
+            return Positions;
+        }
+        
+        #endregion
+    }
+}
