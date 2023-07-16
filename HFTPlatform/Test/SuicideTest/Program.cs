c7e64d872b41651b65dd7f070c9a96167d5b03a8
FerGusMos <fernando.mosca@tradehelm.com>
2023-07-16 15:56:18 -0300

    -suicide test
    Former-commit-id: f7edf9a02a6271acf84654fcae50cf6c7c44115a

---
+++ HFTPlatform/Test/SuicideTest/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using Bittrex;
+using Newtonsoft.Json.Linq;
+
+namespace SuicideTest
+{
+    internal class Program
+    {
+        private static void DoTestConn()
+        {
+            Exchange exch = new Exchange();
+            
+            ExchangeContext ctx= new ExchangeContext()
+            {
+                ApiKey ="",
+                QuoteCurrency = "USDT",
+                Secret = "",
+                Simulate = false
+            };
+            
+            
+           
+            //ctx.QuoteCurrency = "ETH";
+            exch.Initialise(ctx);
+            
+            JObject jMarketData = exch.GetTicker("ETH");
+//            
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            DoTestConn();
+        }
+    }
+}
